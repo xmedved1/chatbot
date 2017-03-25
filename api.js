53b010631f5641a1f6b4b96406f0824e6f2201e9
Cmato <martin.cmarko@gmail.com>
2017-03-25 15:46:57 +0100

    API getEvents by date and city

--- api.js
+++ api.js
@@ -13,7 +13,9 @@ function joinObj(obj) {
     return ret;
 }
 
-function createApiCallUrl(url, params, signature) {
+function createApiCallUrl(url, params) {
+    var signature = crypto.createHmac('sha256', config.api.pkey).update(joinObj(params)).digest('hex');
+
     var linkParams = "";
     Object.keys(params).forEach(function(paramName) {
         linkParams += paramName+"="+params[paramName]+"&";
@@ -23,6 +25,104 @@ function createApiCallUrl(url, params, signature) {
     url = rtrim(url, '/');
 
     return url+"/?"+linkParams;
+}
+
+function searchVenuesCrossPages(params) {
+    return new Promise(function(resolve, reject) {
+        var urlParams = {
+            api_key: config.api.apiKey,
+            page: params.page
+        };
+
+        //make request
+        request({
+            method: 'GET',
+            url: createApiCallUrl(config.api.url+"venues", urlParams),
+        }, function (error, response, body) {
+            if(error)
+            {
+                reject(error);
+            }
+            else if(response.statusCode==403)
+            {
+                reject(new Error('unauthorized'))
+            }
+            else {
+                var data = JSON.parse(body);
+
+                if(typeof data.venues !== "undefined" && data.venues.length > 0) {
+                    data.venues.forEach(function (venue) {
+                        if (venue.city === params.city) {
+                            params.venues[venue.id] = venue;
+                        }
+                    });
+
+                    params.page++;
+                    searchVenuesCrossPages(params).then(function(params) {
+                        resolve(params);
+                    });
+                } else {
+                    resolve(params);
+                }
+            }
+        });
+    });
+}
+
+function searchPerformancesCrossPages(params) {
+    return new Promise(function(resolve, reject) {
+        var urlParams = {
+            api_key: config.api.apiKey,
+            page: params.page,
+            date_from: params.dateFrom,
+            date_to: params.dateTo,
+            venue_id: params.venue
+        };
+
+        //make request
+        request({
+            method: 'GET',
+            url: createApiCallUrl(config.api.url+"performances", urlParams),
+        }, function (error, response, body) {
+            if(error)
+            {
+                reject(error);
+            }
+            else if(response.statusCode==403)
+            {
+                reject(new Error('unauthorized'))
+            }
+            else {
+                var data = JSON.parse(body);
+
+                console.log(data.performances.length);
+                console.log("cinema: "+params.venue+" page: "+params.page);
+
+                if(typeof data.performances !== "undefined" && data.performances.length > 0) {
+
+                    data.performances.forEach(function (performance) {
+                            params.events[performance.event_id] = {id: performance.event_id, title: performance.title};
+                    });
+
+                    params.page++;
+                    searchPerformancesCrossPages(params).then(function(params) {
+                        resolve(params);
+                    });
+                } else {
+                    if(params.venues.length < 1)
+                        resolve(params);
+                    else {
+                        var venue = params.venues.pop();
+                        params.venue = venue;
+                        params.page = 1;
+                        searchPerformancesCrossPages(params).then(function(params) {
+                            resolve(params);
+                        });
+                    }
+                }
+            }
+        });
+    });
 }
 
 // api calls
@@ -38,15 +138,11 @@ module.exports = {
                 api_key: config.api.apiKey,
                 page: 10
             };
-            var signature = crypto.createHmac('sha256', config.api.pkey).update(joinObj(params)).digest('hex');
-            var callUrl = createApiCallUrl(config.api.url+"events", params, signature);
-
-            console.log(callUrl);
 
             //make request
             request({
                 method: 'GET',
-                url: callUrl,
+                url: createApiCallUrl(config.api.url+"events", params),
             }, function (error, response, body) {
                 if(error)
                 {
@@ -66,6 +162,34 @@ module.exports = {
             });
         });
 
+    },
+    getEvents: function(session) {
+        if (!session) return Promise.reject(new Error("Bad parameter"));
+
+        return new Promise(function(resolve, reject) {
+
+            //first get venues is given city
+            searchVenuesCrossPages({page: 1, city: session.userData.city, venues: {} }).then(function(pVenues) {
+                console.log(pVenues.venues);
+                var venuesIds = Object.keys(pVenues.venues);
+                var venue = venuesIds.pop();
+
+                var params = {
+                    page: 1,
+                    dateFrom: session.userData.dateFrom,
+                    dateTo: session.userData.dateTo,
+                    venues: venuesIds,
+                    venue: venue,
+                    events: {}
+                };
+
+                searchPerformancesCrossPages(params).then(function(pEvents) {
+                    resolve(pEvents.events);
+                }, reject);
+
+            }, reject);
+
+        });
     }
 
 };
--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 var api = require('./api');
 
-api.apiTest();
+var session = {};
+session.userData = {
+    city: "Praha 1",
+    dateFrom: "2017-03-26",
+    dateTo: "2017-03-26"
+};
+
+api.getEvents(session).then(function(res) {
+    console.log(res);
+});
