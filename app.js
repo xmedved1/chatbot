961933cfd4beb8f4f4595e1fc12efe22e042d44a
Marek Medveď <xmedved1@triton02.fi.muni.cz>
2017-03-27 16:12:10 +0200

    appId and appPassword change

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ if (config.consoleMode) {
 	connector = new builder.ConsoleConnector().listen();
 } else {
 	connector = new builder.ChatConnector({
-		appId: config.api.pkey,
-		appPassword: config.api.apiKey
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+		
 	});
 }
 
