37931b4713bc99bfd5c0beb20978cb7f74728f4f
Cmato <martin.cmarko@gmail.com>
2017-03-28 19:50:06 +0200

    Include OpenShift configuration

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var config = require('./config');
 var bot = require('./bot');
 var api = require('./api');
 
-
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT || config.port;
+var serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var connector;
 if (config.consoleMode) {
@@ -25,7 +26,7 @@ var chatBot = bot.create(connector);
 if (!config.consoleMode) {
 	// Setup Restify Server
 	var server = restify.createServer();
-	server.listen(config.port || process.env.port || process.env.PORT || 3978, function () {
+	server.listen(serverPort || process.env.port || process.env.PORT || 3978, serverIpAddress, function () {
 		console.log('%s listening to %s', server.name, server.url);
 	});
 	server.use(restify.queryParser());
