24d863707954daa5ad337c50cfcecb7dbb8b42f7
Cmato <martin.cmarko@gmail.com>
2017-03-29 12:53:53 +0200

    Revert "Include OpenShift configuration"
    This reverts commit 37931b4713bc99bfd5c0beb20978cb7f74728f4f.

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var config = require('./config');
 var bot = require('./bot');
 var api = require('./api');
 
-var serverPort = process.env.OPENSHIFT_NODEJS_PORT || config.port;
-var serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 
 var connector;
 if (config.consoleMode) {
@@ -26,7 +25,7 @@ var chatBot = bot.create(connector);
 if (!config.consoleMode) {
 	// Setup Restify Server
 	var server = restify.createServer();
-	server.listen(serverPort || process.env.port || process.env.PORT || 3978, serverIpAddress, function () {
+	server.listen(config.port || process.env.port || process.env.PORT || 3978, function () {
 		console.log('%s listening to %s', server.name, server.url);
 	});
 	server.use(restify.queryParser());
